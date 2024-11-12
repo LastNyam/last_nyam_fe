e0054d0ea4b675378bf401b2ec2c2e508df207e6
oko_jin <dswvgw6412@naver.com>
2024-11-12 23:39:25 +0900

    feat: grey[350] 색상 추가

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -4,6 +4,7 @@ Map<int, Color> grey = {
   100: Color(0xfff3f4f6),
   200: Color(0xffe5e7eb),
   300: Color(0xffd1d5db),
+  350: Color(0xff747b88),
   400: Color(0xff9ca3af),
   500: Color(0xff6b7280),
   600: Color(0xff4b5563),
