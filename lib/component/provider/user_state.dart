8e6243d3c5d18a9e0bc58c75ec4f2affb51ac351
oko_jin <dswvgw6412@naver.com>
2024-11-14 00:00:27 +0900

    feat: userState에 password, phoneNumber 추가

--- lib/component/provider/user_state.dart
+++ lib/component/provider/user_state.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
 class UserState with ChangeNotifier {
+  String password = 'testest';
+  String phoneNumber = '010-1111-1111';
   String userName = '컴공피주먹';
-  int orderCount = 0;
   String profileImage = 'assets/image/profile_image.png';
+  int orderCount = 0;
+
+  void updatePassword(String newPassword) {
+    password = newPassword;
+    notifyListeners();
+  }
+
+  void updatePhoneNumber(String newPhoneNumber) {
+    phoneNumber = newPhoneNumber;
+    notifyListeners();
+  }
 
   void updateUserName(String newName) {
     userName = newName;
     notifyListeners();
   }
 
-  void updateOrderCount(int newCount) {
-    orderCount = newCount;
+  void updateProfileImage(String newImage) {
+    profileImage = newImage;
     notifyListeners();
   }
 
-  void updateProfileImage(String newImage) {
-    profileImage = newImage;
+  void updateOrderCount(int newCount) {
+    orderCount = newCount;
     notifyListeners();
   }
 }
