b265d979b6f35b6ba6a4c50e9ba9bc37053e9864
oko_jin <dswvgw6412@naver.com>
2024-11-14 17:02:16 +0900

    feat: userState의 프로필 이미지를 UI에 보이도록 구현

--- lib/screen/my_page/profile_edit_screen.dart
+++ lib/screen/my_page/profile_edit_screen.dart
@@ -4,8 +4,20 @@ import 'package:last_nyam/screen/my_page/password_change_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:last_nyam/component/provider/user_state.dart';
 import 'package:last_nyam/screen/my_page/nickname_change_screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ProfileEditScreen extends StatelessWidget {
+class ProfileEditScreen extends StatefulWidget {
+  const ProfileEditScreen({super.key});
+
+  @override
+  State<ProfileEditScreen> createState() => _ProfileEditScreenState();
+}
+
+
+class _ProfileEditScreenState extends State<ProfileEditScreen> {
+  File? _profileImage; // 선택된 프로필 이미지
+
   @override
   Widget build(BuildContext context) {
     final userState = Provider.of<UserState>(context);
@@ -39,34 +51,38 @@ class ProfileEditScreen extends StatelessWidget {
             children: [
               SizedBox(height: 20),
               // 프로필 이미지와 편집 아이콘
-              Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundColor: defaultColors['white'],
-                    backgroundImage:
-                        AssetImage('${userState.profileImage}'), // 프로필 이미지 경로
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2.0,
+              GestureDetector(
+                onTap: () => _showPhotoOptions(context), // 옵션 창 표시
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: defaultColors['white'],
+                      backgroundImage: userState.profileImage != null
+                          ? FileImage(userState.profileImage!) // 선택된 이미지
+                          : AssetImage('assets/image/profile_image.png') as ImageProvider, // 기본 이미지 프로필 이미지 경로
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2.0,
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: 15,
+                        backgroundColor: defaultColors['lightGreen'],
+                        child: Icon(
+                          Icons.camera_alt,
+                          size: 18,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                    child: CircleAvatar(
-                      radius: 15,
-                      backgroundColor: defaultColors['lightGreen'],
-                      child: Icon(
-                        Icons.camera_alt,
-                        size: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               SizedBox(height: 20),
               // 닉네임 섹션
@@ -87,9 +103,9 @@ class ProfileEditScreen extends StatelessWidget {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => NicknameChangeScreen(currentNickname: userState.userName),
-                            )
-                        );
+                              builder: (context) => NicknameChangeScreen(
+                                  currentNickname: userState.userName),
+                            ));
                       },
                       child: Row(
                         children: [
@@ -111,7 +127,7 @@ class ProfileEditScreen extends StatelessWidget {
               // 비밀번호 변경 섹션
               ListTile(
                 title: Text(
-                  "비밀번호 변경",
+                  '비밀번호 변경',
                   style: TextStyle(fontSize: 16),
                 ),
                 trailing: Icon(Icons.chevron_right, color: grey[350]),
@@ -120,14 +136,13 @@ class ProfileEditScreen extends StatelessWidget {
                       context,
                       MaterialPageRoute(
                         builder: (context) => PasswordChangeScreen(),
-                      )
-                  );
+                      ));
                 },
               ),
               // 휴대폰 번호 변경 섹션
               ListTile(
                 title: Text(
-                  "휴대폰 번호 변경",
+                  '휴대폰 번호 변경',
                   style: TextStyle(fontSize: 16),
                 ),
                 trailing: Icon(Icons.chevron_right, color: grey[350]),
@@ -139,6 +154,69 @@ class ProfileEditScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  // 이미지 선택 동작
+  Future<void> _pickImageFromGallery() async {
+    final userState = Provider.of<UserState>(context, listen: false);
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    print('프사경로');
+    print(pickedImage!.path);
+    if (pickedImage != null) {
+      userState.updateProfileImage(File(pickedImage.path));
+    }
+  }
+
+  // 기본 이미지 적용
+  void _applyDefaultImage() {
+    final userState = Provider.of<UserState>(context, listen: false);
+    userState.updateProfileImage(null);
+  }
+
+  // 프로필 사진 옵션 선택
+  void _showPhotoOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '프로필 사진 설정',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              ListTile(
+                leading: Icon(Icons.photo_library, color: defaultColors['green']),
+                title: Text('앨범에서 사진 선택'),
+                onTap: () {
+                  Navigator.pop(context); // 옵션 창 닫기
+                  _pickImageFromGallery(); // 갤러리에서 사진 선택
+                },
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.image, color: defaultColors['green']),
+                title: Text('기본 이미지 적용'),
+                onTap: () {
+                  Navigator.pop(context); // 옵션 창 닫기
+                  _applyDefaultImage(); // 기본 이미지 적용
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
